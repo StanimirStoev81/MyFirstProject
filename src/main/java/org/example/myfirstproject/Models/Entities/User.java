3a0c6b6085844ea2eab2d7d76efba882c8991517
Stanimir Stoev <stambqsko@gmail.com>
2025-02-28 16:07:36 +0200

    first commit

---
+++ src/main/java/org/example/myfirstproject/Models/Entities/User.java
@@ -0,0 +1,10 @@
+package org.example.myfirstproject.Models.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class User extends BaseEntity{
+
+}
